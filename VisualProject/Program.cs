0e758d69edbed699713bacc72bd03d725dfd5d2f
sachuu197 <sachinghuge865@gmail.com>
2021-07-29 12:17:36 +0530

    FirstCommit

---
+++ VisualProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VisualProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Boyz!");
+            Console.WriteLine("Hello From MyBr!!!");
+
+
+        }
+    }
+}
